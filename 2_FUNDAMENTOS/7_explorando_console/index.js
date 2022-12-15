4671389e2a7397944b39f41cd74b42670184eb9b
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2022-12-15 16:15:12 -0300

    aula 7 e 8 concluídas

--- 2_FUNDAMENTOS/7_explorando_console/index.js
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -1,7 +1,19 @@
 // mais de um valor
 
 const x = 10
-const y = "algum texto"
+const y = "Ricardo"
 const z = [1,2]
 
 console.log(x, y, z)
+
+// contagem de impressões console.count é sobre isso.
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+
+// contagem de strings
+console.log('o nome é %s, ele é programador', y)
+
+// limpar o console
+setTimeout(() => console.clear(), 2000)
