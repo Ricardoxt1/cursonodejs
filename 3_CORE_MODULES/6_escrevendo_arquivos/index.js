22c188dc6b174d9954dcb7a55cd38b2a948029b3
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-01-06 17:36:51 -0300

    Unindo Fs, Url com Http para realizar petição ao usuario e guardar a informação em arquivo

---
+++ 3_CORE_MODULES/6_escrevendo_arquivos/index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+
+const server = http.createServer(function (req,res) {
+
+    const urlInfo = require('url').parse(req.url, true)
+    const name = urlInfo.query.name  
+
+    if(!name) {
+        fs.readFile('index.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' }) // leitura do arquivo em html
+            res.write(data) //lendo apenas os dados do arquivos e exibindo-o <h1> --- </h1>
+            
+        })
+    } else {
+        fs.writeFile('arquivo.txt', name, function (err, data) {
+            res.writeHead(302, {
+                Location: '/',
+            })
+            return res.end()
+        })
+
+    }
+})
+
+server.listen(port, () => {
+    console.log (`Servidor rodando na porta: ${port}`)
+})
