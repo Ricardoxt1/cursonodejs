cde45206b8204ee777cc3fe87493657d2c6095ef
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-01-17 12:24:49 -0300

    att,

--- 4_NPM_FUNDAMENTAL/4_depedencia_dev/index.js
+++ 4_NPM_FUNDAMENTAL/4_depedencia_dev/index.js
@@ -1,8 +1,8 @@
 const _ = require ('lodash');
 const chalk = require ('chalk');
 
-const a = [1,4,6,7,8,9,10]
-const b = [1,2,3,7,8,9,10,11,12]
+const a = [1,4,6,7,8,9,10,14]
+const b = [1,2,3,7,8,9,10,11,12,13]
 
 const diff = _.difference(a, b)
 
