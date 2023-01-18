73310c2f4adb4a6d1b1303289a884d75a0368224
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-01-18 18:08:37 -0300

    foi realizada a função da criação de conta e um arquivo para guardar futuras alterações e adições

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -42,4 +42,43 @@ function operation() {
 function createAccount() {
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
     console.log(chalk.green('Defina  as opções da sua conta a segur'))
+
+    buildAccount()
+}
+
+function buildAccount(){
+
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para sua conta: '
+        }
+    ]).then(anwser => {    //console.info serve para realizar login pelo nome da conta criada
+        const accountName = anwser['accountName'];
+        console.info(anwser['accountName']) //login com nome da conta
+
+        // se não existir um diretorio fs criará um com a sequência de if
+        if(!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+               
+        }
+
+        //validação, se houver uma conta com mesmo nome não permitirá seguir
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black("Esta conta já existe, por favor, crie uma conta com outro nome!"))
+            buildAccount() 
+            return
+        }
+
+        //criação de um arquivo para guardar dados da conta
+        fs.writeFileSync(`accounts/${accountName}.json`,
+         `{"balance": 0}`,
+          function (err) {
+            console.log(err)
+        })    
+
+        console.log(chalk.green('Parabéns, sua conta foi criada!'))
+        operation()
+    })
+    .catch(err => console.log(err))
 }
