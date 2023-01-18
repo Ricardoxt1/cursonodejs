72cc794837fdbc02e8e243ec0b665a6c018328a6
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-01-18 16:16:20 -0300

    crianção da listagem de opções

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -8,3 +8,31 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 
 console.log("iniciamos o Accounts")
+
+operation()
+
+function operation() {
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'O que você pretende fazer?',
+            choices: 
+            ['Criar Contas',
+            'Consultar Saldo',
+            'Depositar',
+            'Sacar',
+            'Sair']
+        },
+        
+    ])
+
+    // resposta que será dada conforme o desejo do usuario
+    .then(anwser => { 
+        const action = anwser['action']
+
+        console.log(action)
+    }) 
+    .catch( err => console.log(err))
+}
