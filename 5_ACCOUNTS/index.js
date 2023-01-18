dc7ad2c8a90e3e30a635265f579cbea3ff1928b5
Ricardoxt1 <ricardoalexandr_@hotmail.com>
2023-01-18 15:52:46 -0300

    iniciando projeto ACCOUNTS

---
+++ 5_ACCOUNTS/index.js
@@ -0,0 +1,10 @@
+// modulos externos
+
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+
+// modulos internos - cor modules
+
+const fs = require('fs')
+
+console.log("iniciamos o Accounts")
